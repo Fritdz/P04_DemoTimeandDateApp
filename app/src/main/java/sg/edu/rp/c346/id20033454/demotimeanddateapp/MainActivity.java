62793702858e6e1318e39a569b43e62a6238ea45
Fritdz <hafidzzz@hotmail.sg>
2021-05-18 10:55:00 +0800

    final

--- app/src/main/java/sg/edu/rp/c346/id20033454/demotimeanddateapp/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20033454/demotimeanddateapp/MainActivity.java
@@ -3,12 +3,64 @@ package sg.edu.rp.c346.id20033454.demotimeanddateapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatePicker dp;
+    TimePicker tp;
+    Button btnDisplayDate;
+    Button btnDisplayTime;
+    TextView tvDisplay;
+    Button btnReset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dp=findViewById(R.id.datePicker);
+        tp=findViewById(R.id.timePicker);
+        btnDisplayDate=findViewById(R.id.buttonDisplayDate);
+        btnDisplayTime=findViewById(R.id.buttonDisplayTime);
+        tvDisplay=findViewById(R.id.textView);
+        btnReset=findViewById(R.id.reset);
+
+        btnDisplayTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String hour=tp.getCurrentHour().toString();
+                String minute=tp.getCurrentMinute().toString();
+                String output="The time is " + hour + ":" + minute;
+                tvDisplay.setText(output);
+            }
+        });
+
+        btnDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int day = dp.getDayOfMonth();
+                int month = dp.getMonth();
+                int year = dp.getYear();
+
+                String output = String.format("Date is %d/%d/%d", day, month, year);
+                tvDisplay.setText(output);
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dp.updateDate(2020,0,1);
+                tp.setCurrentHour(0);
+                tp.setCurrentMinute(0);
+            }
+        });
     }
 }
